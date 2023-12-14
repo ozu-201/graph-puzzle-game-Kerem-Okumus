2ab95d507d6a9566ca0c34c4616e89d8d596042b
Kerem Okumus <ko028234@ozun.int>
2023-12-14 16:02:12 +0300

    finished, but cmake has a problem

---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by ko028234 on 12/14/2023.
+//
+
+#ifndef GRAPH_PUZZLE_GAME_KEREM_OKUMUS_MAIN_H
+#define GRAPH_PUZZLE_GAME_KEREM_OKUMUS_MAIN_H
+#include "src/List/Graph/Graph.h"
+
+#include <iostream>
+
+class main {
+
+};
+
+
+#endif //GRAPH_PUZZLE_GAME_KEREM_OKUMUS_MAIN_H
